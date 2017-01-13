ba11a02b4dfe2be6e75f5a8f3c66434c81310e2a
dominicboettger <dominic.boettger@inspirationlabs.com>
2017-01-13 18:58:49 +0100

    Added data parameter mapping

---
+++ src/data/MlclDataParam.ts
@@ -0,0 +1,22 @@
+import {injectable} from '@molecuel/di';
+/**
+ * Parameter class for input output functions
+ * 
+ * @export
+ * @class DataParam
+ */
+@injectable
+export class MlclDataParam {
+  public inputParam: string;
+  public targetParam: string;
+  public type: string;
+  public size: number;
+  constructor(inputParam: string, targetParam: string, type: string, size?: number) {
+    this.inputParam = inputParam;
+    this.targetParam = targetParam;
+    this.type = type;
+    if(size) {
+      this.size = size;
+    }
+  }
+}
