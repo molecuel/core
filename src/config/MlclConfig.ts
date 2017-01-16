87fd01586cb7e022b64cea38e61a6ee0fbbc9d1d
dominicboettger <dominic.boettger@inspirationlabs.com>
2017-01-16 19:24:57 +0100

    config handling

---
+++ src/config/MlclConfig.ts
@@ -0,0 +1,37 @@
+import {singleton} from '@molecuel/di';
+import * as fs from 'fs';
+
+@singleton
+export class MlclConfig {
+  private config: any;
+  constructor() {
+    this.readConfig();
+  }
+  readConfig() {
+    let configPath;
+    let environment = 'development';
+    if(process.env.NODE_ENV) {
+      environment = process.env.NODE_ENV;
+    }
+    if(process.env.configfilepath) {
+      configPath = process.env.configfilepath;
+    } else if(process.env.configpath) {
+      configPath = process.env.configpath + '/' + environment + '.json';
+    } else {
+      configPath = process.cwd() + '/config/' + environment + '.json';
+    }
+    try {
+      let readConfig = fs.readFileSync(configPath, 'utf8');
+      this.config = JSON.parse(readConfig);
+    } catch (error) {
+      this.config = {};
+    }
+  }
+  getConfig(path?: string) {
+    if(path) {
+      return this.config[path];
+    } else {
+      return this.config;
+    }
+  }
+}
